3ff6b13ea9fa326d20535b9b78f8833ebf8b1981
Bruce <bruce@redravencomputing.com>
2021-03-31 10:39:02 -0600

    Bind Textfield to Hello Label
    Bind textfield to hello label so when text is change it shows up in label.
    Experimented with debounce to slow text from showing up

--- RxNamer/ViewController.swift
+++ RxNamer/ViewController.swift
@@ -16,9 +16,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var namesLabel: UILabel!
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bind()
+    }
+    
+    func bind() {
+        nameEntryTextField.rx.text
+  //          .debounce(RxTimeInterval.milliseconds(500), scheduler: MainScheduler.instance)
+            .map {
+                if $0  == "" {
+                    return "Type your name below."
+                } else {
+                    return "Hello, \($0!)."
+                }
+            }
+            .bind(to: helloLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 
 
