bf9cef2b008767830c53a10afe101ad27e12407c
Bruce <bruce@redravencomputing.com>
2021-03-31 09:24:18 -0600

    Create View Layout
    Used storyboard to create a view layout with button, labels, and a textfield
    Connect the layout elements to the view controller

--- RxNamer/ViewController.swift
+++ RxNamer/ViewController.swift
@@ -11,7 +11,11 @@ import RxCocoa
 import RxRelay
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var nameEntryTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var namesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
